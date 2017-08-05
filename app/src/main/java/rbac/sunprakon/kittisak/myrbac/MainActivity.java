b45c51fb46a6a35bdfd05ecd2c667de897b59507
inukami <kittisakeye@gmail.com>
2017-08-05 14:34:50 +0700

    Before Lunch

--- app/src/main/java/rbac/sunprakon/kittisak/myrbac/MainActivity.java
+++ app/src/main/java/rbac/sunprakon/kittisak/myrbac/MainActivity.java
@@ -3,11 +3,23 @@ package rbac.sunprakon.kittisak.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rbac.sunprakon.kittisak.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //add fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer,mainFragment)
+                    .commit();
+        }
+    } //main method
+
+} //main class
