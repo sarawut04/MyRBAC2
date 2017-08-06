ef1f4fb2910d892a46db6aa22efc3a59dc2bfbb7
inubari <kittisakeye@gmail.com>
2017-08-06 14:14:16 +0700

    Update 14.14

--- app/src/main/java/rbac/sunprakon/kittisak/myrbac/manager/MyAlert.java
+++ app/src/main/java/rbac/sunprakon/kittisak/myrbac/manager/MyAlert.java
@@ -1,6 +1,10 @@
 package rbac.sunprakon.kittisak.myrbac.manager;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+
+import rbac.sunprakon.kittisak.myrbac.R;
 
 /**
  * Created by Administrator on 6/8/2560.
@@ -8,5 +12,26 @@ import android.content.Context;
 public class MyAlert {
 
     private Context context;
+
+    public MyAlert(Context context) {
+        this.context = context;
+    }
+
+    public void myDialog(String strTitle, String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.mipmap.ic_name);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+
+    }
 
 }
